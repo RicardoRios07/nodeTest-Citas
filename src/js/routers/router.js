05b0bd4559747400c6769b4912d77fb0df47fe80
RickDeuS <riosr3060@gmail.com>
2023-06-21 18:30:34 -0500

    controllers

--- src/js/routers/router.js
+++ src/js/routers/router.js
@@ -1,7 +1,11 @@
-const express=require('express'),
-     router=express.Router()
-     personas=require('../controllers/personasController');
+const express=require('express');
+const router=express.Router();
+const personas=require('../controllers/personasController');
+const preferencias = require('../controllers/preferenciasController');
+const encuentros = require('../controllers/encuentrosConstroller');
 
 router.use("/personas",personas);
+router.use('/preferencias', preferencias);
+router.use('/encuentros', encuentros);
 
 module.exports = router;
